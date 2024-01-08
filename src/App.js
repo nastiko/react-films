ad0e2d642fd8d4603fb015f0ff165b559c7ee0b5
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-01-08 17:25:58 +0000

    started creation a new project, added content for NavBar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-function App() {
+import NavBar from "./components/NavBar"
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function NavBar() {
+    return (
+        <nav>
+            <h1>Fan favorites</h1>
+            <p>This week's top TV and movies</p>
+        </nav>
+    )
+}
