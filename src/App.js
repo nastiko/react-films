be3d3ac9776737a0fc265b471792c74f2153a60e
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-01-09 17:19:08 +0000

    completed creation NavBar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import NavBar from "./components/NavBar"
 
 export default function App() {
   return (
-    <div>
-      <NavBar />
+    <div className="w-full bg-[#23252c]">
+        <div className="w-full lg:w-[992px] bg-[#1a1b21] p-12 mx-auto">
+            <NavBar />
+        </div>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react"
 
 export default function NavBar() {
     return (
-        <nav>
-            <h1>Fan favorites</h1>
+        <nav className="prose prose-h1:text-[32px] prose-h1:leading-[24px] prose-h1:text-[#808080] prose-h1:font-light prose-h1:pl-1.5 prose-h1:mb-0
+        prose-p:text-[18px] prose-p:leading-2 prose-p:text-[#808080] prose-p:font-light prose-p:mt-2 prose-p:mb-0">
+            <div className="flex border-l-[2px] border-l-[#ecd422]">
+                <h1>Fan favorites</h1>
+                <img className="my-0" src="../icons/arrow_right.svg" alt="Icon arrow right"/>
+            </div>
             <p>This week's top TV and movies</p>
         </nav>
     )
