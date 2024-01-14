4688090d6862e84f45f2ca1318eeffb70c81ae71
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-01-14 11:57:37 +0000

    added data and image folder for "Main" component

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        id: 1,
+        title: "The Suicide Squad",
+        description: "A government agent manipulates supervillains to become a part of a dangerous team in exchange for reduced sentences. They get sent to Corto Maltese, where they must destroy a laboratory.",
+        stats: {
+            rating: 7.5,
+            reviewCount: 6
+        },
+    },
+    {
+        id: 2,
+        title: "Shershaah",
+        description: "Vikram Batra, a young man, dreams of becoming a soldier and falls in love with Dimple. Soon after his training, he climbs the military ranks and contributes to India's victory in the Kargil war.",
+        stats: {
+            rating: 7.2,
+            reviewCount: 3
+        },
+    },
+    {
+        id: 3,
+        title: "Free Guy",
+        description: "Guy lives a seemingly peaceful life as a bank teller. However, an encounter with a pretty but mysterious woman makes him realise that he is a non-playable character in a massive online video game.",
+        stats: {
+            rating: 8.8,
+            reviewCount: 10
+        },
+    },
+    {
+        id: 4,
+        title: "Loki",
+        description: "After stealing the Tesseract, Loki comes into contact with a mysterious organization that gives him an ominous ultimatum, either fix the timeline or cease to exist completely.",
+        stats: {
+            rating: 8.2,
+            reviewCount: 5
+        },
+    },
+    {
+        id: 5,
+        title: "Three body problem",
+        description: "The Three-Body Problem is a novel by Chinese science fiction author Liu Cixin, the first in the Remembrance of Earth's Past trilogy—though the series as a whole is often referred to as The Three-Body Problem, or simply as Three-Body.",
+        stats: {
+            rating: 8.2,
+            reviewCount: 5
+        },
+    },
+    {
+        id: 6,
+        title: "Severance",
+        description: "Mark leads a team of office workers whose memories have been surgically divided between their work and personal lives; when a mysterious colleague appears outside of work, it begins a journey to discover the truth about their jobs.",
+        stats: {
+            rating: 8.7,
+            reviewCount: 17
+        },
+    }
+]
